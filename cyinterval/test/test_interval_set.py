49247ca799a97c9afcf35ad08f6d0a60c6da4569
Jason Rudy <jcrudy@gmail.com>
2017-07-22 16:53:19 -0700

    More testing for intersection and union

--- cyinterval/test/test_interval_set.py
+++ cyinterval/test/test_interval_set.py
@@ -54,11 +54,18 @@ def test_intersection():
     interval_set2 = IntervalSet(Interval(.5,1.5))
     assert_equal(interval_set1.intersection(interval_set2).intervals,
                  IntervalSet(Interval(.5,1.,upper_closed=False), Interval(1.,1.5,lower_closed=False)).intervals)
+    interval_set1 = IntervalSet(Interval(0.,1.,upper_closed=False))
+    interval_set2 = IntervalSet(Interval(1.,3.,lower_closed=False))
+    assert_equal(interval_set1.intersection(interval_set2).intervals,
+                 tuple())
     
 def test_union():
     interval_set1 = IntervalSet(Interval(0.,1.,upper_closed=False), Interval(1.,3.,lower_closed=False))
     interval_set2 = IntervalSet(Interval(.5,1.5))
     assert_equal(interval_set1.union(interval_set2).intervals, (Interval(0., 3.),))
+    interval_set1 = IntervalSet(Interval(0.,1.,upper_closed=False))
+    interval_set2 = IntervalSet(Interval(1.,3.,lower_closed=False))
+    assert_equal(interval_set1.union(interval_set2).intervals, (Interval(0., 1., upper_closed=False), Interval(1.,3.,lower_closed=False)))
 
 if __name__ == '__main__':
     import sys
